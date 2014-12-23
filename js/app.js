1f53a7d1aa92061d4356e8c21898ae5886a1b4c7
Elias Jr Mamalias <mamalias23@gmail.com>
2014-12-23 10:23:48 +0800

    add some static pages

--- js/app.js
+++ js/app.js
@@ -15,26 +15,70 @@
 			.when('/choose', {
 				templateUrl: 'page-choose.html',
 				controller: 'ChooseController'
+			})
+			.when('/take-me-home-now', {
+				templateUrl: 'take-me-home-now.html',
+				controller: 'TakeMeHomeNowController'
+			})
+			.when('/pick-me-from-here-now', {
+				templateUrl: 'pick-me-from-here-now.html',
+				controller: 'PickMeFromHereNowController'
+			})
+			.when('/book-a-different-cab-journey', {
+				templateUrl: 'book-a-different-cab-journey.html',
+				controller: 'BookADifferentCabJourneyController'
 			});
 	});
 
 	app.controller('HomeController', function($scope, $rootScope, $location, localStorageService) {
-
+		$rootScope.page_title="Welcome!";
+		$rootScope.backButton = false;
 		$scope.process = function() {
 			$location.path('/choose');
-		};
-
-		$scope.back = function() {
-			$rootScope.pageClass='page-back';
-			history.back();
 		};
 	});
 
 	app.controller('ChooseController', function($scope, $rootScope, $location, localStorageService) {
+		$rootScope.page_title="Choose";
+		$rootScope.backButton = true;
 		$rootScope.pageClass='page';
-		$scope.back = function() {
+		$rootScope.back = function() {
 			$rootScope.pageClass='page-back';
+			$rootScope.backButton = false;
 			$location.path('/');
+		};
+	});
+
+	app.controller('TakeMeHomeNowController', function($scope, $rootScope, $location, localStorageService) {
+		$rootScope.page_title="Take me Home";
+		$rootScope.backButton = true;
+		$rootScope.pageClass='page';
+		$rootScope.back = function() {
+			$rootScope.pageClass='page-back';
+			$rootScope.backButton = false;
+			$location.path('/choose');
+		};
+	});
+
+	app.controller('PickMeFromHereNowController', function($scope, $rootScope, $location, localStorageService) {
+		$rootScope.page_title="Pick me up from here";
+		$rootScope.backButton = true;
+		$rootScope.pageClass='page';
+		$rootScope.back = function() {
+			$rootScope.pageClass='page-back';
+			$rootScope.backButton = false;
+			$location.path('/choose');
+		};
+	});
+
+	app.controller('BookADifferentCabJourneyController', function($scope, $rootScope, $location, localStorageService) {
+		$rootScope.page_title="Book a Cab";
+		$rootScope.backButton = true;
+		$rootScope.pageClass='page';
+		$rootScope.back = function() {
+			$rootScope.pageClass='page-back';
+			$rootScope.backButton = false;
+			$location.path('/choose');
 		};
 	});
 
