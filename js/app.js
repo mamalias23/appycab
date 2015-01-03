95e23bd0f4f6038e4ce7e6a34b2cec2f23edbb24
Elias Jr Mamalias <mamalias23@gmail.com>
2015-01-03 14:30:29 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 (function() {
-	var app = angular.module('appycab', ['LocalStorageModule', 'ngRoute', 'ngAnimate', 'angularValidator', 'ngMap']);
 
-	app.config(function(localStorageServiceProvider, $routeProvider) {
+	var app = angular.module('appycab', ['LocalStorageModule', 'ngRoute', 'ngAnimate', 'angularValidator', 'ngMap', 'ngSanitize', 'timer', 'angular-loading-bar', 'toaster']);
+
+
+	app.constant("APP_TOKEN", 'MpxMxk99c5YfQcuM9gAICKftTu1mFwqO')
+	   .constant('API_BASE_URL','http://appycab.co.uk/appycab-api/public/api/v1/');
+
+	app.config(function(localStorageServiceProvider, $routeProvider, $httpProvider, cfpLoadingBarProvider) {
+
 		localStorageServiceProvider
 			.setPrefix('appycab')
 			.setStorageType('localStorage')
@@ -31,7 +37,21 @@
 			.when('/book-a-different-cab-journey', {
 				templateUrl: 'book-a-different-cab-journey.html',
 				controller: 'BookADifferentCabJourneyController'
+			})
+			.when('/make-booking', {
+				templateUrl: 'make-booking.html',
+				controller: 'MakeBookingController'
 			});
+
+		// We need to setup some parameters for http requests
+	    // These three lines are all you need for CORS support
+	    // $httpProvider.defaults.useXDomain = true;
+	    // $httpProvider.defaults.withCredentials = false;
+	    // $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+	    // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+	    cfpLoadingBarProvider.latencyThreshold = 10;
+
 	});
 
 	app.controller('HomeController', function($scope, $rootScope, $location, localStorageService, AppyCabService) {
@@ -49,6 +69,7 @@
 		$rootScope.backButton = false;
 		$rootScope.hasDetails = AppyCabService.hasDetails();
 		$scope.client = {
+			app_id:localStorageService.get('app_id'),
 			fname:localStorageService.get('fname'),
 			lname:localStorageService.get('lname'),
 			email:localStorageService.get('email'),
@@ -60,15 +81,15 @@
 
 		$scope.process = function() {
 
-			localStorageService.set('fname', $scope.client.fname);
-			localStorageService.set('lname', $scope.client.lname);
-			localStorageService.set('email', $scope.client.email);
-			localStorageService.set('mobile', $scope.client.mobile);
-			localStorageService.set('address', $scope.client.address);
-			localStorageService.set('lat', $scope.client.lat);
-			localStorageService.set('lng', $scope.client.lng);
+			var detail = AppyCabService.processDetail($scope.client);
 
-			$location.path('/choose');
+			detail.success(function(response) {
+
+				localStorageService.set('app_id', response.data.app_id);
+
+				$location.path('/choose');
+			});
+			
 		};
 
 		var getFormattedAddress = function(pos) {
@@ -125,6 +146,7 @@
 		};
 
 		$scope.client = {
+			app_id:localStorageService.get('app_id'),
 			fname:localStorageService.get('fname'),
 			lname:localStorageService.get('lname'),
 			email:localStorageService.get('email'),
@@ -160,6 +182,11 @@
 			$scope.map.markers[0].setPosition(event.latLng);
 			getFormattedAddress(event.latLng);
 		}
+
+		$scope.process = function() {
+			//AppyCabService.takeMeHomeNowProcess($scope.client);
+			$location.path('/make-booking');
+		}
 	});
 
 	app.controller('PickMeFromHereNowController', function($scope, $rootScope, $location, localStorageService, AppyCabService) {
@@ -186,8 +213,37 @@
 		};
 	});
 
+	app.controller('MakeBookingController', function($scope, $rootScope, $location, localStorageService, AppyCabService, toaster) {
+		$rootScope.hasDetails = AppyCabService.hasDetails();
+		$rootScope.page_title="Booked";
+		$rootScope.backButton = false;
+		$rootScope.pageClass='page';
+
+		$scope.callbackTimer={};
+		$scope.callbackTimer.finished=function(){
+			$location.path('/choose');
+			toaster.pop({type: 'success', title: "Booked!", body:"You've successfully booked a cab"});
+	        $scope.$apply();
+	    };
+	});
+
 	
-	app.factory("AppyCabService", function(localStorageService) {
+	app.factory("AppyCabService", function(localStorageService, APP_TOKEN, API_BASE_URL, $http, $sanitize) {
+
+		var sanitizeDetailsData = function (data) {
+            return {
+                app_id: $sanitize(data.app_id),
+                fname: $sanitize(data.fname),
+                lname: $sanitize(data.lname),
+                email: $sanitize(data.email),
+                mobile: $sanitize(data.mobile),
+                address: $sanitize(data.address),
+                lat: $sanitize(data.lat),
+                lng: $sanitize(data.lng),
+                _token: APP_TOKEN
+            };
+        };
+
 		return {
 			hasDetails: function() {
 				if(localStorageService.get('fname')!==null 
@@ -202,6 +258,38 @@
 				} else {
 					return false;
 				}
+			},
+
+			processDetail: function(data) {
+
+				localStorageService.set('app_id', data.app_id);
+				localStorageService.set('fname', data.fname);
+				localStorageService.set('lname', data.lname);
+				localStorageService.set('email', data.email);
+				localStorageService.set('mobile', data.mobile);
+				localStorageService.set('address', data.address);
+				localStorageService.set('lat', data.lat);
+				localStorageService.set('lng', data.lng);
+
+				var detail = $http.post('http://appycab.co.uk/appycab-api/public/api/v1/details', sanitizeDetailsData(data));
+
+				return detail;
+			},
+
+			takeMeHomeNowProcess: function(data) {
+
+				localStorageService.set('app_id', data.app_id);
+				localStorageService.set('fname', data.fname);
+				localStorageService.set('lname', data.lname);
+				localStorageService.set('email', data.email);
+				localStorageService.set('mobile', data.mobile);
+				localStorageService.set('address', data.address);
+				localStorageService.set('lat', data.lat);
+				localStorageService.set('lng', data.lng);
+
+				var detail = $http.post('http://appycab.co.uk/appycab-api/public/api/v1/take-me-home-now', sanitizeDetailsData(data));
+
+				return detail;
 			}
 		};
 	});
